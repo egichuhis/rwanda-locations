cc909c90fa0ecff6d147c82de47816066ea67a2c
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-10-09 19:12:58 +0300

    Add styles for header

--- src/components/Districts.jsx
+++ src/components/Districts.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
+import DistrictCard from './DistrictCard';
+import ''
 
 const Districts = () => (
   <div>
-    <h1
-      style={{
-        fontSize: 15,
-        background: '#34548b',
-        color: 'var(--bs-body-bg)',
-        paddingLeft: 5,
-        paddingBottom: 5,
-        paddingTop: 5,
-        marginBottom: 0,
-      }}
-    >
+    <h1 className="header-txt">
       STATS BY DISTRICTS
     </h1>
+    <div className="row g-0" style={{ background: '#3f62a3' }}>
+      <DistrictCard />
+    </div>
   </div>
 );
 
