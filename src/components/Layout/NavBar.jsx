58d13335564a3945938bd02f906bf66e1ffc177c
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-10-12 12:36:45 +0300

    Add Back Arrow button for home navigation

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -1,39 +1,49 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
+
 import Logo from '../../imgs/rwanda.png';
 
-const NavBar = () => (
-  <nav className="navbar navbar-expand-md bg-body py-3">
-    <div className="container">
-      <a className="navbar-brand d-flex align-items-center" href="/">
-        <span>
-          <img
-            src={Logo}
-            alt="Logo"
-            width={40}
-            height={40}
-            style={{ paddingRight: 5 }}
-          />
-          Rwanda
-        </span>
-      </a>
-      <button
-        className="navbar-toggler"
-        data-bs-toggle="collapse"
-        data-bs-target="#navcol-1"
-        type="button"
-      >
-        <span className="visually-hidden">Toggle navigation</span>
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div
-        id="navcol-1"
-        className="collapse navbar-collapse d-flex justify-content-end show"
-      >
-        <ul className="navbar-nav me-auto" />
+const NavBar = () => {
+  const location = useLocation();
+  return (
+    <nav className="navbar navbar-expand-md bg-body py-3">
+      <div className="container">
+        {location.pathname !== '/' && (
+          <Link to="/">
+            <i className="fas fa-arrow-left fs-4" />
+          </Link>
+        )}
+        <a className="navbar-brand d-flex align-items-center" href="/">
+          <span>
+            <img
+              alt="Logo"
+              src={Logo}
+              width={40}
+              height={40}
+              style={{ paddingRight: 5 }}
+            />
+            Rwanda
+          </span>
+        </a>
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navcol-1"
+        >
+          <span className="visually-hidden">Toggle navigation</span>
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          id="navcol-1"
+          className="collapse navbar-collapse d-flex justify-content-end"
+        >
+          <ul className="navbar-nav me-auto" />
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
-);
+  );
+};
 
 export default NavBar;
